e16b3129ff9512738ccde29d998df05fb070fbf8
T.Krivonos <tatyana.krivonos@binomo.com>
2021-08-29 12:49:25 +0300

    module2-task3

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,6 +1,7 @@
 import Swiper from "swiper";
 
 export default () => {
+  const body = document.querySelector(`body`);
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
@@ -17,14 +18,19 @@ export default () => {
         },
         on: {
           slideChange: () => {
+            body.classList.remove(...Array.from(body.classList).filter((v) => v.startsWith(`slide`)));
             if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+              body.classList.add(`slide1-active`);
             } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
+              body.classList.add(`slide2-active`);
             } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
+              body.classList.add(`slide3-active`);
             } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
+              body.classList.add(`slide4-active`);
             }
           },
           resize: () => {
@@ -51,14 +57,19 @@ export default () => {
         },
         on: {
           slideChange: () => {
+            body.classList.remove(...Array.from(body.classList).filter((v) => v.startsWith(`slide`)));
             if (storySlider.activeIndex === 0) {
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
+              body.classList.add(`slide1-active`);
             } else if (storySlider.activeIndex === 2) {
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
+              body.classList.add(`slide2-active`);
             } else if (storySlider.activeIndex === 4) {
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
+              body.classList.add(`slide3-active`);
             } else if (storySlider.activeIndex === 6) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
+              body.classList.add(`slide4-active`);
             }
           },
           resize: () => {
