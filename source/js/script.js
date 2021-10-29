14217b1e3b71fb313af84fa17bdeb739a60bb133
T.Krivonos <tatyana.krivonos@binomo.com>
2021-10-29 12:10:13 +0300

    animation done

--- source/js/script.js
+++ source/js/script.js
@@ -46,7 +46,26 @@ formFieldResult2.addEventListener(`animationend`, () => {
   formFieldResult2.classList.add(`animation-done`);
 });
 
-// const pathP = document.querySelector(`#firstWordP`);
-// console.log(pathP.getTotalLength());
 
+// ! temporary solution for result titles
+const tempoResultBtnS = document.querySelectorAll(`.js-show-result`);
+const resultSection1 = document.querySelector(`#result`);
+const resultSection2 = document.querySelector(`#result2`);
+const resultSection3 = document.querySelector(`#result3`);
+const resultImg1 = resultSection1.querySelector(`#win-img-main`).src;
+const resultImg2 = resultSection2.querySelector(`#win-img-second`).src;
+const resultImg3 = resultSection3.querySelector(`#mistake-img`).src;
 
+const resultSections = [resultSection1, resultSection2, resultSection3];
+const resultImages = [resultImg1, resultImg2, resultImg3];
+
+tempoResultBtnS.forEach((btn) => {
+  btn.addEventListener(`click`, () => {
+    resultSections.forEach((section, i) => {
+      if (section.classList.contains(`screen--show`)) {
+        const img = section.querySelector(`.result-title-img`);
+        img.src = `${resultImages[i]}?` + new Date().getTime();
+      }
+    });
+  });
+});
